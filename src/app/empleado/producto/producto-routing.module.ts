951b7585f2f606347466864f4dab15eb5bfb4ebb
JEfrainDR <djoseefrainr@gmail.com>
2021-06-08 12:11:22 -0600

    Tabla de productos
    Adición de un componente para tabla de productos. De momento es solo un cascarón de lo que será la tabla, pero más adelante se desarrollará más a detalle.

--- src/app/empleado/producto/producto-routing.module.ts
+++ src/app/empleado/producto/producto-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormProductoComponent } from './components/form-producto/form-producto.component';
+import { TableProductoComponent } from './components/table-producto/table-producto.component';
 
 const routes: Routes = [
   { path: 'form-producto', component: FormProductoComponent },
+  { path: 'table-producto', component: TableProductoComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'form-producto' }
 ];
 
