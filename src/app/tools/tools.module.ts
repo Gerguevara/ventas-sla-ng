646d6101b31b8686bbaed7116f4bfec7980b3c48
JEfrainDR <djoseefrainr@gmail.com>
2021-06-13 12:35:45 -0600

    Solucion de error
    Solución de error de fallo en reconocimiento en el modulo Tools

--- src/app/tools/tools.module.ts
+++ src/app/tools/tools.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DialogMessageComponent } from './components/dialog-message/dialog-message.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
+import { DialogSpinnerComponent } from './components/dialog-spinner/dialog-spinner.component';
+
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { DialogMessageComponent } from './components/dialog-message/dialog-message.component';
-import { DialogSpinnerComponent } from './components/dialog-spinner/dialog-spinner.component';
-import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @NgModule({
   declarations: [
-    DialogMessageComponent,
     DialogSpinnerComponent,
+    DialogMessageComponent,
     PaginatorComponent
   ],
   imports: [
@@ -21,8 +22,8 @@ import { PaginatorComponent } from './components/paginator/paginator.component';
     MatDialogModule,
     MatButtonModule,
     MatInputModule,
-    MatPaginatorModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatPaginatorModule
   ]
 })
 export class ToolsModule { }
