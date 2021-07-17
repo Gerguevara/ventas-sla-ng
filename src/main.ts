6ac3f0951504a233bf70a38afadd659a3e8f9622
Rubén Pérez <49503500+rubper@users.noreply.github.com>
2021-07-17 15:04:56 -0600

    font configurable

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,27 @@ if (environment.production) {
   enableProdMode();
 }
 
+
+if(!document.getElementById('fontLink')) {
+  let appFontArray = environment.appFont.split(' ');
+  let appFontOutput = '';
+  if(appFontArray.length > 1){
+    appFontArray.forEach((word,index)=>{
+      appFontArray[index] = word.trim();
+      appFontOutput = appFontOutput.concat(appFontArray[index]);
+      if(appFontArray[index+1]){
+        appFontOutput = appFontOutput.concat('+');
+      }
+    })
+  } else {
+    appFontOutput = appFontArray[0].trim();
+  }
+  console.log(appFontOutput);
+  var link = document.createElement('link');
+  link.id = 'fontLink';
+  link.rel = 'stylesheet';
+  link.href = `https://fonts.googleapis.com/css2?family=${appFontOutput}:wght@300;400;500&display=swap`;
+  document.head.appendChild(link);
+}
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
