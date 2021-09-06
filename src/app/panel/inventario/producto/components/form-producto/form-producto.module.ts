55f53729b1e0dc4bdd40099ca473d713acd06add
JEfrainDR <djoseefrainr@gmail.com>
2021-09-06 16:28:58 -0600

    Traslado de componentes de formulario al nuevo módulo de formulario de productos

--- src/app/panel/inventario/producto/components/form-producto/form-producto.module.ts
+++ src/app/panel/inventario/producto/components/form-producto/form-producto.module.ts
@@ -2,13 +2,60 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormProductoRoutingModule } from './form-producto-routing.module';
+import { ProductoDesignFormComponent } from './components/producto-design-form/producto-design-form.component';
+import { ProductoGeneralFormComponent } from './components/producto-general-form/producto-general-form.component';
+import { ProductoInventarioFormComponent } from './components/producto-inventario-form/producto-inventario-form.component';
+import { NgxMatFileInputModule } from '@angular-material-components/file-input';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { ToolsModule } from '@tools/tools.module';
+import { NgxPermissionsModule } from 'ngx-permissions';
+import { FormProductoComponent } from './form-producto.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FormProductoComponent,
+    ProductoGeneralFormComponent,
+    ProductoDesignFormComponent,
+    ProductoInventarioFormComponent,
+  ],
   imports: [
     CommonModule,
-    FormProductoRoutingModule
+    FormProductoRoutingModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatSelectModule,
+    NgxMatFileInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    ToolsModule,
+    MatSnackBarModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatAutocompleteModule,
+    NgxPermissionsModule.forChild()
   ]
 })
 export class FormProductoModule { }
